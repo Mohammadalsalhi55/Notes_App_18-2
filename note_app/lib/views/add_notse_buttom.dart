3243da41588622ac31ff97acf2699a9fab06e94c
Mohammadalsalhi55 <ramaalsalhi1@gmail.com>
2024-01-24 17:48:20 +0300

    add new field

--- note_app/lib/views/add_notse_buttom.dart
+++ note_app/lib/views/add_notse_buttom.dart
@@ -6,14 +6,23 @@ class AddNotesButtom extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Padding(
+    return const Padding(
       padding: const EdgeInsets.symmetric(horizontal: 16),
       child: Column(
         children: [
           SizedBox(
             height: 32,
           ),
-          CustomTextField(),
+          CustomTextField(
+            hint: 'Title',
+          ),
+          SizedBox(
+            height: 16,
+          ),
+          CustomTextField(
+            hint: 'Content',
+            maxlines: 5,
+          ),
         ],
       ),
     );
