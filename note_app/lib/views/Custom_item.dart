aadb559cb0b14160a7806976a2bdcbafef083d6f
Mohammadalsalhi55 <ramaalsalhi1@gmail.com>
2024-01-31 01:27:45 +0300

    creat edit view

--- note_app/lib/views/Custom_item.dart
+++ note_app/lib/views/Custom_item.dart
@@ -2,48 +2,57 @@
 
 import 'package:flutter/material.dart';
 import 'package:font_awesome_flutter/font_awesome_flutter.dart';
+import 'package:note_app/views/edit_new_view.dart';
 
 class Noteitems extends StatelessWidget {
   const Noteitems({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return Container(
-      padding: const EdgeInsets.only(top: 24, bottom: 24, left: 16),
-      decoration: BoxDecoration(
-          color: const Color(0XffFFCC80),
-          borderRadius: BorderRadius.circular(16)),
-      child: Column(crossAxisAlignment: CrossAxisAlignment.end, children: [
-        ListTile(
-          title: const Text(
-            "Flutter tips",
-            style: TextStyle(color: Colors.black, fontSize: 26),
+    return GestureDetector(
+      onTap: () {
+        Navigator.push(context, MaterialPageRoute(builder: (context) {
+          return const Editnewview();
+        }));
+      },
+      child: Container(
+        padding: const EdgeInsets.only(top: 24, bottom: 24, left: 16),
+        decoration: BoxDecoration(
+            color: const Color(0XffFFCC80),
+            borderRadius: BorderRadius.circular(16)),
+        child: Column(crossAxisAlignment: CrossAxisAlignment.end, children: [
+          ListTile(
+            title: const Text(
+              "Flutter tips",
+              style: TextStyle(color: Colors.black, fontSize: 26),
+            ),
+            subtitle: Padding(
+              padding: const EdgeInsets.only(top: 16, bottom: 16),
+              child: Text(
+                "build your career with mohammad alsalhi",
+                style: TextStyle(
+                    color: Colors.black.withOpacity(.5), fontSize: 18),
+              ),
+            ),
+            trailing: IconButton(
+              onPressed: () {},
+              icon: const Icon(
+                FontAwesomeIcons.trash,
+                size: 24,
+                color: Colors.black,
+              ),
+            ),
           ),
-          subtitle: Padding(
-            padding: const EdgeInsets.only(top: 16, bottom: 16),
+          Padding(
+            padding: const EdgeInsets.only(right: 24),
             child: Text(
-              "build your career with mohammad alsalhi",
+              "jan 23_2024",
               style:
-                  TextStyle(color: Colors.black.withOpacity(.5), fontSize: 18),
+                  TextStyle(color: Colors.black.withOpacity(.4), fontSize: 14),
             ),
           ),
-          trailing: IconButton(
-            onPressed: () {},
-            icon: const Icon(
-              FontAwesomeIcons.trash,
-              size: 24,
-              color: Colors.black,
-            ),
-          ),
-        ),
-        Padding(
-          padding: const EdgeInsets.only(right: 24),
-          child: Text(
-            "jan 23_2024",
-            style: TextStyle(color: Colors.black.withOpacity(.4), fontSize: 14),
-          ),
-        ),
-      ]),
+        ]),
+      ),
     );
   }
 }
---
+++ note_app/lib/views/edit_note.dart
@@ -0,0 +1,13 @@
+import 'package:flutter/material.dart';
+import 'package:note_app/views/edit_new_view.dart';
+
+class EditNote extends StatelessWidget {
+  const EditNote({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Editnewview(),
+    );
+  }
+}
