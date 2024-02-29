d1e48db71ef3c2e208d8c078150abd30b028020e
Mohammadalsalhi55 <ramaalsalhi1@gmail.com>
2024-02-29 13:46:28 +0300

    create note mode

---
+++ note_app/lib/models/note_mode.dart
@@ -0,0 +1,11 @@
+class NoteMode {
+  final String title;
+  final String subTitle;
+  final String date;
+  final int color;
+  NoteMode(
+      {required this.title,
+      required this.subTitle,
+      required this.date,
+      required this.color});
+}
